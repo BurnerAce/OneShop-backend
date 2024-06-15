003cf958a25a3410495b857d18270789a1dfa8bf
BurnerAce <hanzalasharique04@gmail.com>
2024-06-15 17:43:15 +0530

    done

--- api/index.js
+++ api/index.js
@@ -61,6 +61,7 @@ app.get('/signup', (req, res) => {
     res.send('Hello World!');
 });
 app.post('/signup', async (req, res) => {
+    console.log("Done");
     res.header("Access-Control-Allow-Origin", "https://one-shop-burnerace.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
