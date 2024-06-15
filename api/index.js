22c22f46dcc34ece7bdc3333c4558c9cb9be3a49
BurnerAce <hanzalasharique04@gmail.com>
2024-06-15 17:22:49 +0530

    index

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ const NodeGeocoder = require('node-geocoder');
 const corsOptions = {
     origin: 'https://one-shop-burnerace.vercel.app',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
     credentials: 'true'
   };
 
